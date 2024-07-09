380bd811aef5917bb2838e9161531ef12d99022a
LauDuhalde <duhalde.ulloa.laura@gmail.com>
2024-07-09 18:08:53 -0400

    first commit

---
+++ src/component.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import React from 'react'
+
+export default function Component() {
+  const [text, setText] = useState()
+  const [updated, setUpdated] = useState()
+  
+  const textOnChange = (event) => {
+    setText(event.target.value)
+  }
+
+  const buttonOnClick = (event) => {
+    setUpdated(text)
+  }
+
+  return (
+    <div>
+      <input type='text' value={text} onChange={textOnChange}></input>
+      <button onClick={buttonOnClick}>Actualizar</button>
+      <p>Texto input: {text}</p>
+      <p>Texto actualizado: {updated}</p>
+    </div>
+  )
+}
